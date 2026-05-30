cb151ba3f27d4ec548bc7ba3ed0ca9d237502ec7
Saharsh Saravanan <mailidofsaharshsaravanan@gmail.com>
2026-05-30 16:26:34 -0400

    jsondata made so far

--- jsondata.py
+++ jsondata.py
@@ -1,13 +1,14 @@
 import json
 
 data = {"name": "SS", "prime_candidate": True}
+def write(data)
+    with open("output.json", "w") as f:
+        json.dump(data, f, indent=4)
 
-with open("output.json", "w") as f:
-    json.dump(data, f, indent=4)
-import json
-
-with open("data.json", "r") as f:
-    data = json.load(f)
+def read():
+    with open("data.json", "r") as f:
+        data = json.load(f)
+        return data
 
 print(data)
 
