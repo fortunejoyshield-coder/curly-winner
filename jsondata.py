8ddefd30d3b3121f4015e0c3fcb941acb666c7d8
Saharsh Saravanan <mailidofsaharshsaravanan@gmail.com>
2026-05-30 16:46:43 -0400

    jsondata made so far

--- jsondata.py
+++ jsondata.py
@@ -1,14 +1,29 @@
 import json
 
 data = {"name": "SS", "prime_candidate": True}
-def write(data)
+
+def write(data):
     with open("output.json", "w") as f:
         json.dump(data, f, indent=4)
 
 def read():
     with open("data.json", "r") as f:
-        data = json.load(f)
-        return data
+        return json.load(f)
 
-print(data)
+def stringify(kind):
+    if kind == 1:
+        return "TF="
+    elif kind == 2:
+        return "P+1F="
+    else:
+        return "PollardP-1 ="
 
+def format(assignment):
+    data1 = stringify(assignment.type)
+    return data1 + "," + str(assignment.p) + "," + str(assignment.limit)
+
+def formatcores(cpu_num, work):
+    data = ""
+    for x in range(1, cpu_num + 1):
+        data += f"Worker {x} doing assignment {work[x]}\n"
+    return data
